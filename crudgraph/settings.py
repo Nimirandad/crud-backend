f646ed55de0c64f17b9507eec2b20ace1ed454df
ni.mirandad <nicolas.miranda@trebol-it.cl>
2020-10-12 16:38:46 -0300

    csfr y cors anadido

--- crudgraph/settings.py
+++ crudgraph/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'compania',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crudgraph.wsgi.application'
 
+CORS_ORIGIN_WHITELIST = ('http://localhost:8080',)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
